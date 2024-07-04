ec00a70ee1f0d458b127d103ea41975f550f2859
Maxim Semikov <maxim.semikov87@gmail.com>
2024-07-04 23:45:05 +0500

    fix MakeAnAppointmentBtn

--- src/components/buttons/makeAnAppointmentBtn.tsx
+++ src/components/buttons/makeAnAppointmentBtn.tsx
@@ -1,14 +1,12 @@
 
 export const MakeAnAppointmentBtn = () => (
     <div
-        className={'group absolute bottom-[-4rem] left-0 size-52 border border-rumi-light rounded-full' +
-            ' flex' +
-            ' justify-center items-center cursor-pointer hover:bg-rumi-light'}
+        className={'group absolute bottom-[-4rem] left-0 size-[300px] border border-rumi-light rounded-full' +
+            ' flex justify-center items-center cursor-pointer hover:border-4'}
     >
         <div
-            className={'size-44 bg-rumi-light rounded-full text-white flex justify-center items-center' +
-                ' group-hover:border' +
-                ' border-white'}
+            className={'size-56 bg-rumi-light rounded-full text-white flex justify-center items-center' +
+                ' uppercase text-2xl tracking-widest'}
         >
             Записаться
         </div>
