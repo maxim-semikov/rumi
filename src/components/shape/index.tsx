20ad769d115bdfeacf91c32a0f2b05f257094385
Maxim Semikov <maxim.semikov87@gmail.com>
2024-07-04 23:44:51 +0500

    fix shape

--- src/components/shape/index.tsx
+++ src/components/shape/index.tsx
@@ -3,18 +3,18 @@ import './shapeStyles.css'
 
 export const Shape = ({children}:React.PropsWithChildren) => (
     <div className={'text-white my-10'}>
-        <div className={'h-10 flex'}>
+        <div className={'h-[50px] flex'}>
             <div className={'round-out'}/>
 
-            <div className={'grow bg-rumi-light rounded-ss-2xl rounded-se-2xl'}/>
+            <div className={'grow bg-rumi-light rounded-ss-[26px] rounded-se-[40px]'}/>
         </div>
 
-        <div className={'bg-rumi-light rounded-ss-2xl rounded-ee-2xl pb-10 px-10'}>
+        <div className={'bg-rumi-light rounded-ss-[40px] rounded-ee-[40px] pb-10 px-10'}>
             {children}
         </div>
 
-        <div className={'h-10 flex'}>
-            <div className={'bg-rumi-light rounded-es-2xl rounded-ee-2xl grow'}/>
+        <div className={'h-[50px] flex'}>
+            <div className={'bg-rumi-light rounded-es-[40px] rounded-ee-[26px] grow'}/>
             <div className={'round-out-second'}/>
         </div>
     </div>
