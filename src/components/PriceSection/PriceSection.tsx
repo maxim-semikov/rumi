8e2f96600247ed012de96a86e0100b40fea251c7
Maxim Semikov <maxim.semikov87@gmail.com>
2024-07-11 10:43:33 +0500

    fix

--- src/components/PriceSection/PriceSection.tsx
+++ src/components/PriceSection/PriceSection.tsx
@@ -1,16 +1,12 @@
 "use client"
 import React from "react";
 import {elMessiri} from "@/app/fonts";
-import {Button} from "@/components/buttons";
 
 export const PriceSection = () => {
 
     return (
         <section id='price' className={'my-20 bg-bgColor'}>
             <h2 className={`${elMessiri.className} text-4xl uppercase text-center mb-10`}>Услуги</h2>
-
-            <Button />
-
         </section>
     )
 }
